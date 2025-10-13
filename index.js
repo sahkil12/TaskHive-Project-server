26789bac0c239dd9a4adc9fb208c22db866cb71b
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-10-13 22:19:20 +0600

    mongodb conection with server successfully

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 const app = express(); 
+require('dotenv').config()
 const port = process.env.PORT || 3000;
-
+const { MongoClient } = require('mongodb');
+// middleware
 app.use(cors())
 app.use(express.json())
+// mongo connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gr8kgxz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db('TaskHive');
+    const taskCollection = db.collection('tasks');
+    const userCollection = db.collection('users');
+
+    // post task
+                    
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+  
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res)=>{
     res.send("TaskHive Make There Server Successfully")
